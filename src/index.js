39e347d1ef6a4bbc0205c516d99ae241d2209826
benwalker <benwalker@simple.com>
2020-05-09 14:18:47 -0700

    update custom domain

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/welcome-to-player-sheet">
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
