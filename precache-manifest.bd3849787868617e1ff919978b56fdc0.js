eabd79862aef7f63835c1407f8b55937ea30cabc
benwalker <benwalker@simple.com>
2020-05-09 18:25:40 -0700

    Updates

---
+++ precache-manifest.bd3849787868617e1ff919978b56fdc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe89c711a9a8217d547b0aef958e69e",
+    "url": "/welcome-to-player-sheet/index.html"
+  },
+  {
+    "revision": "9b3ff9a04de7c299ac34",
+    "url": "/welcome-to-player-sheet/static/js/2.4631d828.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/welcome-to-player-sheet/static/js/2.4631d828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8b163665aed93a43dd",
+    "url": "/welcome-to-player-sheet/static/js/main.a72b5a54.chunk.js"
+  },
+  {
+    "revision": "cbe897661450c2d60455",
+    "url": "/welcome-to-player-sheet/static/js/runtime-main.211d4765.js"
+  }
+]);
