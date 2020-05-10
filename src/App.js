c8ff36b3a9d3d7e87427777b4ab0d5b450840d71
benwalker <benwalker@simple.com>
2020-05-10 14:50:31 -0700

    baseline, no sheet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from "react";
 import ImageMapper from "react-image-mapper";
-import logo from "./logo.svg";
 import "./App.css";
 import map from "./map";
 
-const url = "./player-sheet.png";
+const url = "./sheet.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <ImageMapper
-          src={url}
-          map={map}
-          fillColor={"#B4E3DE"}
-          width={1157}
-          height={1157}
-        />
-      </header>
+      <ImageMapper
+        src={url}
+        map={map}
+        fillColor={"#B4E3DE"}
+        width={1157}
+        height={1157}
+      />
     </div>
   );
 }
